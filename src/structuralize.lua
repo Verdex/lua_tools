364094e513f3bb217eb20cb2c081b5028111f698
David L. Inman <davidlevere@gmail.com>
2023-11-22 20:12:34 -0500

    inc prog for exact list

--- src/structuralize.lua
+++ src/structuralize.lua
@@ -16,105 +16,151 @@ local function is_wild(t)
     return t.kind == "wild"
 end
 
+local function exact_table(t)
+    return {table = table, type = "pattern", kind = "exact_table"}
+end
+
+local function is_exact(t) 
+    return t.kind == "exact_table"
+end
+
+local function merge(t1, t2)
+    local r = {}
+    for _, v in ipairs(t1) do
+        r[#r + 1] = v
+    end
+    for _, v in ipairs(t2) do
+        r[#r + 1] = v
+    end
+    return r
+end
+
+--[[
+local function product(cs, results) 
+    results = results or {}
+    if #cs ~= 0 then
+        local c = table.remove(cs)
+        for captures in c do
+            if #captures ~= 0 then 
+                product(cs, merge(results, captures))
+            end
+        end
+    else
+        coroutine.yield(results)
+    end
+end
+--]]
+
+local function match_exact(ps, data, results)
+    results = results or {}
+    if #ps == 0 then
+        coroutine.yield(results)
+    else
+        local p = table.remove(ps)
+        local d = table.remove(data)
+        local c = match(p, d)
+        for output in c do
+            match_exact(ps, data, merge(results, output))
+        end
+    end
+end
+
 local function match(pattern, data)
     assert(type(pattern) ~= "table" or pattern.type == "pattern")
     return coroutine.wrap(function() 
         if type(pattern) ~= "table" then
-            coroutine.yield({pattern == data, data})
+            coroutine.yield({{pattern == data, data}})
         elseif is_capture(pattern) then
-            coroutine.yield({pattern.name, data})
+            coroutine.yield({{pattern.name, data}})
         elseif is_wild(pattern) then
-            coroutine.yield({true, data})
+            coroutine.yield({{true, data}})
+        elseif is_exact(pattern) and #pattern.table == #data then
+            match_exact(pattern.table, data)
+        else 
+            -- TODO failure
+            error("Unrecognized pattern")
+        end
 
-        else -- table
-            local r = {}
-            for k, v in pairs(pattern) do 
-                if data[k] then
-                    r[#r+1] = match(v, data[k])
-                end
-            end
-            
-
-        -- table
         -- path
         -- list path
         -- and/or ?
-        -- template ?
-        -- pattern function ?
         -- matcher function ?
-        end
     end)
 end
 
 
 -- should capture
 r = match(capture 'x', 40)
-a, b = unpack(r())
-assert(a == 'x')
-assert(b == 40)
+o = r()
+assert(#o == 1)
+assert(o[1][1] == 'x')
+assert(o[1][2] == 40)
 
 -- should match: nil, boolean, number, string
 r = match(1, 1)
-a, b = unpack(r())
-assert(a)
-assert(b == 1)
+o = r()
+assert(#o == 1)
+assert(o[1][1])
+assert(o[1][2] == 1)
 
 r = match(nil, nil)
-a, b = unpack(r())
-assert(a)
-assert(b == nil)
+o = r()
+assert(#o == 1)
+assert(o[1][1])
+assert(o[1][2] == nil)
 
 r = match(false, false)
-a, b = unpack(r())
-assert(a)
-assert(b == false)
+o = r()
+assert(#o == 1)
+assert(o[1][1])
+assert(o[1][2] == false)
 
 r = match("xstring", "xstring")
-a, b = unpack(r())
-assert(a)
-assert(b == "xstring")
+o = r()
+assert(#o == 1)
+assert(o[1][1])
+assert(o[1][2] == "xstring")
 
 -- should fail match: nil, boolean, number, string
 r = match(2, 1)
-a, b = unpack(r())
-assert(not a)
-assert(b == 1)
+o = r()
+assert(#o == 1)
+assert(not o[1][1])
+assert(o[1][2] == 1)
 
 r = match("", nil)
-a, b = unpack(r())
-assert(not a)
-assert(b == nil)
+o = r()
+assert(#o == 1)
+assert(not o[1][1])
+assert(o[1][2] == nil)
 
 r = match(true, false)
-a, b = unpack(r())
-assert(not a)
-assert(b == false)
+o = r()
+assert(#o == 1)
+assert(not o[1][1])
+assert(o[1][2] == false)
 
 r = match("ystring", "xstring")
-a, b = unpack(r())
-assert(not a)
-assert(b == "xstring")
+o = r()
+assert(#o == 1)
+assert(not o[1][1])
+assert(o[1][2] == "xstring")
 
 -- should match wild
 r = match(wild(), { x = 1})
-a, b = unpack(r())
-assert(a)
-assert(b.x == 1)
+o = r()
+assert(#o == 1)
+assert(o[1][1])
+assert(o[1][2].x == 1)
 
--- should product
---[[
-function ww(x)
-    return coroutine.wrap(function() 
-        coroutine.yield(1 + x)
-        coroutine.yield(2 + x)
-        coroutine.yield(3 + x)
-    end)
-end
+-- should match list
+r = match(exact_table{capture 'x', 2, capture 'y'}, {1, 2, 3})
 
-blarg = coroutine.wrap(function() product({ww(1), ww(2), ww(3)}) end)
 
-r = blarg()
 
---]]
+-- should match structure
+
+-- should match table
+
 
 print("ok")
